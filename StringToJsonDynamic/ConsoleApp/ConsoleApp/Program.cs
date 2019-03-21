66b54e3322d6f354a7a02078250f9dcbc8f5267e
feng xiong <xiong11feng@sina.com>
2019-03-21 18:33:30 +0800

    json字符串转换成dynamic对象

---
+++ StringToJsonDynamic/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonStringTest = "{\"Nonce\": \"HspZE5PuzDtr1s93\",\"Sign\": \"CCC812D82CD08F11A649427FC6EDCE64\",\"BankType\": \"RakutenPay\",\"Result\": \"SUCCESS\", \"ResultDesc\": \"支払が完了しました。レシート発行する場合は、印刷ボタンを押してください。\",\"TradeState\": \"SUCCESS\"}";
+            var jsonTestObj = JObject.Parse(jsonStringTest);
+            if (jsonTestObj["Result"] != null)
+            {
+                Console.WriteLine(jsonTestObj["Result"].ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
